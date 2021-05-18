cb77f982f39d1089f0d7cb0b3beae8f3da5cf9c8
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2021-05-18 14:30:37 -0300

    :tada: first commit

---
+++ assets/js/main.js
@@ -0,0 +1,76 @@
+/*  Menu Show e Hidden */
+
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+
+/*  Show  */
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+
+/*  hidden  */
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+
+/*  Remove Menu  */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*  Scroll Section Active Link  */
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop -50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight){
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.add('active')
+        }
+        else {
+            document.querySelector('.nav__menu a[href*='+ sectionId +']').classList.remove('active')
+        }
+    })
+}
+
+
+/* ---------------------------------------------------------------------- */
+
+
+/* ativando link */
+const linkColor = document.querySelectorAll('.nav__link')
+function colorLink() {
+    linkColor.forEach(l=> l.classList.remove('active'))
+    this.classList.add('active')
+}
+linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+
+/* collapse menu */
+const linkCollapse = document.getElementsByClassName('collapse__link')
+var i
+
+for(i=0;i<linkCollapse.length;i++) {
+    linkCollapse[i].addEventListener('click', function(){
+        const collapseMenu = this.nextElementSibling
+        collapseMenu.classList.toggle('showCollapse')
+
+        const rotate = collapseMenu.previousElementSibling
+        rotate.classList.toggle('rotate')
+    })
+}
