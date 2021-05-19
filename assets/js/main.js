5ebbeb34b6721be7da3ef095151e3619cb036064
GuilhermeMota20 <guidroidbr.2007@gmail.com>
2021-05-19 15:49:18 -0300

    :tada: final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,17 +50,6 @@ function scrollActive(){
 
 
 /* ---------------------------------------------------------------------- */
-
-
-/* ativando link */
-const linkColor = document.querySelectorAll('.nav__link')
-function colorLink() {
-    linkColor.forEach(l=> l.classList.remove('active'))
-    this.classList.add('active')
-}
-linkColor.forEach(l => l.addEventListener('click', colorLink))
-
-
 /* collapse menu */
 const linkCollapse = document.getElementsByClassName('collapse__link')
 var i
